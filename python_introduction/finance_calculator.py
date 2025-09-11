13a51967ab4a4e2c6178c44e71afa3171a336fea
Samuel Anokye <andrewssamuelanokye@gmail.com>
2025-09-11 18:35:21 +0000

    useless codes removed

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,11 +1,10 @@
 monthly_income = int(input("enter your monthly income:")) 
 total_monthly_expenses = int(input("Enter your total monthly expenses:")) 
-difference_result = monthly_income - total_monthly_expenses
-print ("Monthly savings is", difference_result)
-annual_interest_rate = 5/100
-projected_Savings = difference_result * 12
-projected_mothly_savings_rate = difference_result * 12 * 0.05
-annual_Savings_Projection_Calc = projected_Savings + projected_mothly_savings_rate
-print ("projected annual savings is", annual_Savings_Projection_Calc)
+monthly_savings = monthly_income - total_monthly_expenses
+print ("Monthly savings is", monthly_savings)
+projected_Savings = (monthly_savings * 12 + (monthly_savings * 12 * 0.05))
+print ("Projected savings after one year, with interest is", projected_Savings)
+
+
 
 
