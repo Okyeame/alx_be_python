e575d4df72f8b35cc3732bce216e5a733b8e172f
Samuel Anokye <andrewssamuelanokye@gmail.com>
2025-09-11 18:22:06 +0000

    finance_calculator script

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,11 @@
+monthly_income = int(input("Enter your monthly income")) 
+total_monthly_expenses = int(input("Enter your total monthly expenses")) 
+difference_result = monthly_income - total_monthly_expenses
+print ("Monthly savings is", difference_result)
+annual_interest_rate = 5/100
+projected_Savings = difference_result * 12
+projected_mothly_savings_rate = difference_result * 12 * 0.05
+annual_Savings_Projection_Calc = projected_Savings + projected_mothly_savings_rate
+print ("projected annual savings is", annual_Savings_Projection_Calc)
+
+
