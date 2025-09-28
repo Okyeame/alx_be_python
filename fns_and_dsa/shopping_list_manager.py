2658077cc094b85beb4264b5fae36d9f5ceb71bf
Samuel Anokye <andrewssamuelanokye@gmail.com>
2025-09-28 16:28:31 +0100

    modified:   shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,7 +1,7 @@
 # shopping_list_manager.py
 
 def display_menu():
-    print("\nShopping List Manager")
+    print("Shopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
     print("3. View List")
@@ -18,7 +18,7 @@ def main():
             item = input("Enter the item to add: ").strip()
             if item:
                 shopping_list.append(item)
-                print(f"'{item}' has been added to your shopping list.")
+                print("'" + item + "' has been added to your shopping list.")
             else:
                 print("You cannot add an empty item.")
         
@@ -26,15 +26,15 @@ def main():
             item = input("Enter the item to remove: ").strip()
             if item in shopping_list:
                 shopping_list.remove(item)
-                print(f"'{item}' has been removed from your shopping list.")
+                print("'" + item + "' has been removed from your shopping list.")
             else:
-                print(f"'{item}' not found in your shopping list.")
+                print("'" + item + "' not found in your shopping list.")
         
         elif choice == '3':
             if shopping_list:
-                print("\nYour Shopping List:")
+                print("Your Shopping List:")
                 for idx, item in enumerate(shopping_list, start=1):
-                    print(f"{idx}. {item}")
+                    print(str(idx) + ". " + item)
             else:
                 print("Your shopping list is empty.")
         
