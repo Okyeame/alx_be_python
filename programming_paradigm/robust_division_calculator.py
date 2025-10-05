fd76654af3eb79f8201cefafa3f2fde39e163719
Samuel Anokye <andrewssamuelanokye@gmail.com>
2025-10-05 20:35:39 +0100

    modifie 1

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -14,7 +14,7 @@ def safe_divide(numerator, denominator):
 
         # Perform division
         result = num / den
-        return f"The result of the division is {result:.2f}"
+        return f"The result of the division is {result:.1f}"
 
     except ValueError:
         return "Error: Please enter numeric values only."
